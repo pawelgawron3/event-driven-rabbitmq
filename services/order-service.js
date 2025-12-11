cbb2c2b9903d5e8c6976ca0e7b138b5b4728186f
pawelgawron3 <148467604+pawelgawron3@users.noreply.github.com>
2025-12-11 12:19:07 +0100

    Add sendOrderCreatedEvent() function

---
+++ services/order-service.js
@@ -0,0 +1,24 @@
+const amqp = require("amqplib");
+
+async function sendOrderCreatedEvent(orderId, customerId) {
+  const connection = await amqp.connect("amqp://localhost");
+  const channel = await connection.createChannel();
+  const queue = "order_created_queue";
+
+  await channel.assertQueue(queue, { durable: true });
+
+  const event = {
+    orderId,
+    customerId,
+    status: "created",
+  };
+
+  channel.sendToQueue(queue, Buffer.from(JSON.stringify(event)), {
+    persistent: true,
+  });
+
+  console.log(`Order ${orderId} created and event send`);
+
+  await channel.close();
+  await connection.close();
+}
