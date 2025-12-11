66576a0c7cce63de1267ae8dc114626aaed0cba4
pawelgawron3 <148467604+pawelgawron3@users.noreply.github.com>
2025-12-11 12:31:54 +0100

    Add processOrderCreated() and sendPaymentProcessedEvent() functions

---
+++ services/payment-service.js
@@ -0,0 +1,34 @@
+const amqp = require("amqplib");
+
+async function processOrderCreated(msg) {
+  const event = JSON.parse(msg.content.toString());
+  const orderId = event.orderId;
+
+  console.log(`Processing payment for order ${orderId}`);
+
+  setTimeout(() => {
+    sendPaymentProcessedEvent(orderId);
+  }, 1000);
+}
+
+async function sendPaymentProcessedEvent(orderId) {
+  const connection = await amqp.connect("amqp://localhost");
+  const channel = connection.createChannel();
+  const queue = "payment_processed_queue";
+
+  await channel.assertQueue(queue, { durable: true });
+
+  const event = {
+    orderId,
+    status: "paid",
+  };
+
+  channel.sendToQueue(queue, Buffer.from(JSON.stringify(event)), {
+    persistent: true,
+  });
+
+  console.log(`Payment processed for order ${orderId}. Event sent`);
+
+  await channel.close();
+  await connection.close();
+}
